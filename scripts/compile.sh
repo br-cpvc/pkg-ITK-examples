7608a3631657e501f06b47d4f89a579b4cc25ded
Christian P. V. Christoffersen <88783438+br-cpvc@users.noreply.github.com>
2022-12-19 15:13:01 +0000

    initial commit

---
+++ scripts/compile.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+#set -x
+script_dir=$(dirname "$0")
+cwd=`pwd`
+
+old_method=true
+if [[ "$old_method" == true ]]; then
+    # use a hardcoded version of itk, downloaded
+    # as a tar ball and compiled from source
+    #itk_version="4.5.0"
+    #itk_version="4.7.2"
+    itk_version=4.8.2
+    itk_dir_prefix="InsightToolkit"
+    outputdir=$2
+    source ${script_dir}/dwn_itk.sh $itk_dir_prefix $itk_version $outputdir
+
+    itk_dir="$outputdir/$itk_dir_prefix-$itk_version"
+else
+    # use git submodule checkout version of itk,
+    # needs to have added itk as git submodule deps/ITK
+    itk_dir="deps/ITK"
+fi
+
+# build dependencies
+build_script=$1
+source $build_script $itk_dir
